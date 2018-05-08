170e05811e0003875794f9b90e1b117e962e3382
polyakovigor <igor26.polyakov@gmail.com>
2018-05-08 19:45:59 +0300

    Add import csv feature

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,10 @@
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
+
+  def import
+    User.import(params[:file])
+    redirect_to root_url, notice: 'Data imported!'
+  end
 end
