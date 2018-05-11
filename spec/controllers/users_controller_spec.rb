477fa9d6a8175b43c18ad12d0135bf193908b0bb
polyakovigor <igor26.polyakov@gmail.com>
2018-05-11 10:18:14 +0300

    Test pass

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,4 +7,24 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to be_success
     end
   end
+
+  describe 'POST #import' do
+    context 'with correct file' do
+      it 'returns success' do
+        @file = fixture_file_upload(Rails.root.join('public', 'test.csv'))
+        post :import, params: { file: @file }
+        expect(response).to redirect_to(users_path)
+        expect(flash[:notice]).to match(/Data imported!/)
+      end
+    end
+
+    context 'with another file format' do
+      it 'returns error' do
+        @file = fixture_file_upload(Rails.root.join('public', 'robots.txt'))
+        post :import, params: { file: @file }
+        expect(response).to redirect_to(root_path)
+        expect(flash[:notice]).to match(/Invalid format - txt! Must be - CSV!/)
+      end
+    end
+  end
 end
